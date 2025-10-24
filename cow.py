cf9da474cc83a673e31d416fb1f820f56a2f0e71
Advik01-byte <iamadvikgoyal@gmail.com>
2025-10-24 09:52:43 +0530

    Update cow.py

--- cow.py
+++ cow.py
@@ -1,10 +1,11 @@
 import cowsay as cow
 
 # Print a cow saying "Hello, World!"
-cow.cow("Hello, World!")
+cow.cow("Good Mooooorning!")
 
 # Print a dragon saying "Python is awesome!"
 cow.dragon("Python is awesome!")
 
 # Print a turtle saying "Keep calm and code on!"
+
 cow.turtle("Keep calm and code on!")
